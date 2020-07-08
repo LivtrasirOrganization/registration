816b05c49288b53af6dedc8fc550420733fc4ee0
Your Name <you@example.com>
2020-07-08 21:26:18 +0300

    Изменили файлы css html js

--- ScriptJS0807.js
+++ ScriptJS0807.js
@@ -1,4 +1,14 @@
 'use strict';
 window.addEventListener('load', () => {
+let form = document.getElementById('form1');
+    form.addEventListener('submit', ()=> {
+        let access = confirm('Вы уверены в надежности данных?');
+        if (access === true){
+            form.submit();
+        }
+        else{
+            alert('Данные не надежные. Отправка заблокирована');
+        }
+    })
 
-})
+});
